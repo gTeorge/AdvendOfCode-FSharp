3049151fb5b3d6d76ca5c5f3fff3e374cf2ca71f
petetmic <michal.peterka@tieto.com>
2020-06-25 14:37:17 +0200

    Task 3-2

--- 2015-03.fsx
+++ 2015-03.fsx
@@ -2,25 +2,44 @@ open System
 open System.IO
 
 let source = File.ReadAllText(__SOURCE_DIRECTORY__ + "/2015-03-input.txt")
+             |> Seq.toList
 
+let updatePosition (pos: int * int) (dir: char) =
+    match dir with
+    | c when c = '<' -> (fst pos - 1, snd pos)
+    | c when c = '>' -> (fst pos + 1, snd pos)
+    | c when c = '^' -> (fst pos, snd pos + 1)
+    | c when c = 'v' -> (fst pos, snd pos - 1)
+    | _ -> pos
 
 module partOne =
-    let visits (directions: string) =
-        let directions = directions |> Seq.toList
-        let rec travel dir pos (ls: (int*int) list)=
+    let visits directions =
+        let rec travel dir pos (ls: (int*int) list) =
             match dir with
             | [] -> ls
-            | h::t when h = '<' -> travel t (fst pos - 1, snd pos) (ls|> List.append [(fst pos - 1, snd pos)])
-            | h::t when h = '>' -> travel t (fst pos + 1, snd pos) (ls|> List.append [(fst pos + 1, snd pos)])
-            | h::t when h = '^' -> travel t (fst pos, snd pos + 1) (ls|> List.append [(fst pos, snd pos + 1)])
-            | h::t when h = 'v' -> travel t (fst pos, snd pos - 1) (ls|> List.append [(fst pos, snd pos - 1)])
-            | h::t -> travel t pos ls
+            | h::t -> let newPosition = updatePosition pos h
+                      travel t newPosition (ls|> List.append [newPosition])
         travel directions (0,0) [(0,0)]
 
-//module partTwo = 
-    // Split directions to santa and robot
+module partTwo = 
+    let visits directions =
+        let rec travel dir posSanta posRobo (ls: (int * int) list) =
+            match dir with
+            | [] -> ls
+            | [s] -> let newSantaPosition = updatePosition posSanta s
+                     travel [] newSantaPosition posRobo (ls |> List.append [newSantaPosition])
+            | s::r::t -> let newSantaPosition = updatePosition posSanta s
+                         let newRoboPosition = updatePosition posRobo r
+                         travel t newSantaPosition newRoboPosition (ls |> List.append [newSantaPosition; newRoboPosition])
+        travel directions (0,0) (0,0) [(0,0)]
+
 
 partOne.visits source
+|> List.distinct
+|> List.length
+|> printfn "%d"
+
+partTwo.visits source
 |> List.distinct
 |> List.length
 |> printfn "%d"
